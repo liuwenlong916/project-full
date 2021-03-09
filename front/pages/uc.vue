dc3736485478e9ddcc3aba08ad83a4c968244f28
liuwenlong916 <1141657011@qq.com>
2021-03-09 23:02:49 +0800

    布隆过滤器切片

--- front/pages/uc.vue
+++ front/pages/uc.vue
@@ -20,6 +20,25 @@
       ></el-progress>
     </div>
     <div>
+      <div class="cube-container" :style="{ width: cubeWidth + 'px' }">
+        <div class="cube" v-for="chunk in chunks" :key="chunk.name">
+          <div
+            :class="{
+              uploading: chunk.progress > 0 && chunk.progress < 100,
+              success: chunk.progress == 100,
+              error: chunk.progress < 0
+            }"
+          >
+            <i
+              class="el-icon-loading"
+              style="color:$f56c6c"
+              v-if="chunk.progress < 100 && chunk.progress > 0"
+            ></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
       <el-button @click="uploadFile">上传</el-button>
     </div>
   </div>
@@ -37,10 +56,25 @@ export default {
   data() {
     return {
       file: null,
-      uploadProgress: 0,
+      // uploadProgress: 0,
       chunks: [],
       hashProgress: 0
     };
+  },
+  computed: {
+    cubeWidth() {
+      //平方根向上取整
+      return Math.ceil(Math.sqrt(this.chunks.length)) * 16;
+    },
+    uploadProgress() {
+      if (!this.file || this.chunks.length) {
+        return 0;
+      }
+      const loaded = this.chunks
+        .map(item => item.chunk.size * item.progress)
+        .reduce(acc, cur => acc + cur, 0);
+      return parseInt(((loaded / this.file.size) * 100).toFixed(2));
+    }
   },
   methods: {
     bindEvent() {
@@ -117,6 +151,39 @@ export default {
         window.requestIdleCallback(workloop);
       });
     },
+    async calculateHashSample() {
+      //布隆过滤器
+      return new Promise(resolve => {
+        const spark = new sparkMD5.ArrayBuffer();
+        const reader = new FileReader();
+        const file = this.file;
+        const size = file.size;
+        const offset = 2 * 1024 * 1024;
+        //首尾2M，中间去前中后各两个字节
+        let chunks = [file.slice(0, offset)];
+        let cur = offset;
+        while (cur < size) {
+          if (cur + offset > size) {
+            //到末尾取最后2M
+            chunks.push(file.slice(size - offset, size));
+          } else {
+            let midd = cur + offset / 2;
+            let end = cur + offset;
+            chunks.push(file.slice(cur, cur + 2));
+            chunks.push(file.slice(midd, midd + 2));
+            chunks.push(file.slice(end - 2, end));
+          }
+          cur += offset;
+        }
+
+        reader.readAsArrayBuffer(new Blob(chunks));
+        reader.onload = e => {
+          spark.append(e.target.result);
+          this.hashProgress = 100;
+          resolve(spark.end());
+        };
+      });
+    },
 
     async uploadFile() {
       console.log("----");
@@ -125,22 +192,53 @@ export default {
       //   return;
       // }
       //根据size生成文件切片
-      this.chunks = this.createFileChunk(this.file);
-      const hash = await this.calculateHashWorker();
-      console.log(hash);
-      const hash1 = await this.calculateHashIdle();
-      console.log(hash1);
-      const form = new FormData();
-      form.append("name", "file");
-      form.append("file", this.file);
-      const res = await this.$http.post("/uploadfile", form, {
-        onUploadProgress: progress => {
-          this.uploadProgress = Number(
-            ((progress.loaded / progress.total) * 100).toFixed(2)
-          );
-        }
+      const chunks = this.createFileChunk(this.file);
+      // const hash = await this.calculateHashWorker();
+      // console.log(hash);
+      // const hash1 = await this.calculateHashIdle();
+      // console.log(hash1);
+      const hash = await this.calculateHashSample();
+      this.chunks = chunks.map((chunk, index) => {
+        const name = hash + "-" + index;
+        return {
+          hash,
+          name,
+          index,
+          chunk: chunk.file
+        };
       });
-      console.log(res);
+      await this.uploadChunks();
+      // const form = new FormData();
+      // form.append("name", "file");
+      // form.append("file", this.file);
+      // const res = await this.$http.post("/uploadfile", form, {
+      //   onUploadProgress: progress => {
+      //     this.uploadProgress = Number(
+      //       ((progress.loaded / progress.total) * 100).toFixed(2)
+      //     );
+      //   }
+      // });
+      // console.log(res);
+    },
+    async uploadChunks() {
+      const requests = this.chunks
+        .map((chunk, index) => {
+          const form = new FormData();
+          form.append("hash", chunck.hash);
+          form.append("name", chunck.name);
+          form.append("index", chunck.index);
+          form.append("chunk", chunck.chunk);
+          return from;
+        })
+        .map((form, index) => {
+          this.$http.poost("/uploadfile", form, {
+            onUploadProgress: progress => {
+              this.chunks[index].progress = Numberr(
+                ((progress.loaded / progress.total) * 100).toFixed(2)
+              );
+            }
+          });
+        });
     },
     async isImage(file) {
       return (
@@ -199,5 +297,23 @@ export default {
   line-height: 100px;
   text-align: center;
   border: 2px dashed #eee;
+}
+
+.cube {
+  width: 14px;
+  height: 14px;
+  line-height: 12px;
+  border: 1px black solid;
+  background: #eee;
+  float: left;
+}
+.success {
+  background: green;
+}
+.uploading {
+  background: blue;
+}
+.error {
+  background: red;
 }
 </style>
